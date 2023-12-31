9a53110eb5cd49e06b1af58cff8f025806f72025
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-12-30 21:54:28 -0600

    feat(projects/05-weather-app): Add the config for tailwindcss

--- projects/05-weather-app/src/main.js
+++ projects/05-weather-app/src/main.js
@@ -1,8 +1,9 @@
-import './app.css'
 import App from './App.svelte'
 
+const root = document.getElementById('app')
+
 const app = new App({
-  target: document.getElementById('app'),
+  target: root,
 })
 
 export default app
