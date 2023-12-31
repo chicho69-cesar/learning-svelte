69da4ec388190c35faebea82ce273c2f0a7820a9
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-12-31 13:27:50 -0600

    feat(projects/00-movie-searcher): Finish the project movie searcher

--- projects/00-movie-searcher/src/App.svelte
+++ projects/00-movie-searcher/src/App.svelte
@@ -1,52 +1,49 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
-  import styles from './Test.module.css'
+	import Search from './lib/Search.svelte'
+	import Movies from './lib/Movies.svelte'
+
+  const API_KEY = import.meta.env.VITE_API_KEY
+  const API_URL = `https://www.omdbapi.com/?apikey=${API_KEY}`
+
+  let promise
+  let searchInput = ''
+
+  const handleSubmit = value => {
+		if (value.length <= 2) {
+			alert('Introduce más de 2 letras para buscar')
+    	return
+		}
+
+		searchInput = value
+  }
+
+  const fetchMovies = async (search) => {
+    const response = await fetch(`${API_URL}&s=${search}`)
+    if (!response.ok) return []
+
+    const json = await response.json()
+    const { Response, Search } = await json
+
+		if (Response === 'False') return []
+	
+    return Search.map(({ Title, Year, Poster }) => ({
+			title: Title,
+			year: Year,
+			poster: Poster
+		}))
+  }
+
+  $: if (searchInput.length > 2) {
+    promise = fetchMovies(searchInput)
+  }
 </script>
 
-<h2 class={styles.test}>
-  Prueba
-</h2>
-
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+  <Search onSubmit={handleSubmit} />
 
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  {#await promise}
+    <p>Cargando...</p>
+  {:then movies}
+    <Movies movies={movies} />
+  {/await}
 </main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
---
+++ projects/00-movie-searcher/src/lib/Search.svelte
@@ -0,0 +1,20 @@
+<script>
+  let value = ''
+  export let onSubmit
+</script>
+
+<form on:submit|preventDefault={() => onSubmit(value)}>
+  <label>
+    Introduce tu película:
+
+    <input
+      name='movie'
+      placeholder='Avengers, The Matrix, etc.'
+      bind:value={value}
+    />
+  </label>
+
+  <button type='submit'>
+    Buscar película
+  </button>
+</form>
