d57aa303c0c0a90027a1ade5538d54cbb3d35a61
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-01-01 15:05:15 -0600

    feat(projects/06-crm-clients): Configure axios, db and datatypes

---
+++ projects/06-crm-clients/src/types/clients.ts
@@ -0,0 +1,10 @@
+export interface Client {
+  name:     string
+  lastName: string
+  email:    string
+  phone:    string
+  business: string
+  role:     string
+  state:    boolean
+  id:       number
+}
