e94ea2084712ce124b509ea0ab00cdba190145a5
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-01-01 21:33:30 -0600

    feat(projects/07-cocktail): Add config files and dependencies

--- projects/07-cocktail/src/main.ts
+++ projects/07-cocktail/src/main.ts
@@ -1,8 +1,9 @@
-import './app.css'
 import App from './App.svelte'
 
+const target = document.getElementById('app') as HTMLElement
+
 const app = new App({
-  target: document.getElementById('app'),
+  target
 })
 
 export default app
