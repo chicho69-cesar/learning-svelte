dcb7ce0696832b202c12e19dee357d78d681cd05
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-01-02 13:15:26 -0600

    feat(projects/07-cocktail): Configure routing for the project

--- projects/07-cocktail/src/App.svelte
+++ projects/07-cocktail/src/App.svelte
@@ -1,9 +1,55 @@
 <script lang='ts'>
   import './app.css'
+  import { Router, Route } from 'svelte-routing'
+
+  import Header from './components/Header.svelte'
+  import Modal from './components/Modal.svelte'
+  import Notification from './components/Notification.svelte'
+  import Home from './routes/Home.svelte'
+  import Favorites from './routes/Favorites.svelte'
+
+  export let url = ''
 </script>
 
-<main>
-  <h1 class='my-6 text-5xl font-bold text-center'>
-    Hola Mundo!!!
-  </h1>
-</main>
+<Router {url}>
+  <div>
+    <Header />
+
+    <main class="container mx-auto mt-10">
+      <Route
+        path='/'
+        component={Home}
+      />
+
+      <Route
+        path='/favorites'
+        component={Favorites}
+      />
+    </main>
+
+    <Modal />
+    <Notification />
+  </div>
+</Router>
+
+<style>
+  /* width */
+  :global(::-webkit-scrollbar) {
+    width: 5px;
+  }
+
+  /* Track */
+  :global(::-webkit-scrollbar-track) {
+    background: #fff;
+  }
+
+  /* Handle */
+  :global(::-webkit-scrollbar-thumb) {
+    background: #ea7437;
+  }
+
+  /* Handle on hover */
+  :global(::-webkit-scrollbar-thumb:hover) {
+    background: #d35f25;
+  }
+</style>
