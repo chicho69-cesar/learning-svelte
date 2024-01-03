bd15f756c8aa19078780038f594a3e8b2eeaf6f9
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-01-02 22:05:32 -0600

    feat(projects/07-cocktail): Finish home page shown all recipes in search

---
+++ projects/07-cocktail/src/components/Recipe.svelte
@@ -0,0 +1,31 @@
+<script lang='ts'>
+  import type { DrinkElement as DrinkElementOne } from '../types/drink'
+  import type { DrinkElement as DrinkElementTwo } from '../types/drinks'
+  import { drinksStore } from '../stores/drinks.store'
+
+  export let recipe: DrinkElementOne | DrinkElementTwo
+</script>
+
+<div class='border rounded shadow-lg'>
+  <div class='overflow-hidden'>
+    <img
+      class='transition-transform hover:scale-125 hover:rotate-2'
+      src={recipe.strDrinkThumb}
+      alt={`Imagen de ${recipe.strDrink}`}
+    />
+  </div>
+
+  <div class='p-5'>
+    <h2 class='text-2xl font-extrabold truncate'>
+      {recipe.strDrink}
+    </h2>
+
+    <button
+      type='button'
+      on:click={() => drinksStore.selectDrink(recipe.idDrink ?? '')}
+      class='w-full mt-5 text-lg font-bold text-white bg-orange-400 rounded hover:bg-orange-500'
+    >
+      Ver Receta
+    </button>
+  </div>
+</div>
--- projects/07-cocktail/src/routes/Home.svelte
+++ projects/07-cocktail/src/routes/Home.svelte
@@ -1,16 +1,25 @@
 <script lang='ts'>
   import Header from '../components/Header.svelte'
-  import { drinksStore } from '../stores/drinks.store'
+  import Recipe from '../components/Recipe.svelte'
+  import { drinksStore, thereIsRecipes } from '../stores/drinks.store'
 </script>
 
 <Header />
 
 <main class='container mx-auto mt-10'>
-  <p>
-    {$drinksStore.search.name}
-  </p>
-  
-  <p>
-    {$drinksStore.search.category}
-  </p>
+  <div>
+    <h1 class='text-6xl font-extrabold'>Recetas</h1>
+
+    {#if $thereIsRecipes}
+      <div class='grid grid-cols-1 gap-2 my-10 md:grid-cols-2 lg:grid-cols-3'>
+        {#each $drinksStore.recipes.drinks as drink, i (drink.idDrink)}
+          <Recipe recipe={drink} />
+        {/each}
+      </div>
+    {:else}
+      <p class='my-10 text-2xl text-center'>
+        Realice una búsqueda
+      </p>
+    {/if}
+  </div>
 </main>
